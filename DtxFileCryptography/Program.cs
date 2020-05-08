ee016d7d29d313421dbd775c0baaed37be405bdd
Dariush Tasdighi <dariush.tasdighi@hotmail.com>
2020-05-08 16:03:56 +0430

    Step 2

--- DtxFileCryptography/Program.cs
+++ DtxFileCryptography/Program.cs
@@ -1,9 +1,44 @@
-﻿namespace DtxFileCryptography
+﻿//namespace DtxFileCryptography
+//{
+//	class Program
+//	{
+//		static void Main()
+//		{
+//			System.Console.WriteLine("Hello, World!");
+//		}
+//	}
+//}
+
+namespace DtxFileCryptography
 {
 	class Program
 	{
-		static void Main(string[] args)
+		static int Main(string[] args)
 		{
+			if (args == null)
+			{
+				System.Console.WriteLine("[args] is null!");
+				return 1;
+			}
+
+			if (args.Length == 0)
+			{
+				System.Console.WriteLine("[args] length is zero!");
+				return 2;
+			}
+
+			if (args.Length != 2)
+			{
+				System.Console.WriteLine("[args] length should be 2!");
+				return 3;
+			}
+
+			foreach (string arg in args)
+			{
+				System.Console.WriteLine(arg);
+			}
+
+			return 0;
 		}
 	}
 }
